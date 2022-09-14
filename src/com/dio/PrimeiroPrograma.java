3a7d67731f54cde34f56bc846f5a3262f81b71b7
nadjanesantana <nadjanesantana.dev@gmail.com>
2022-09-14 19:32:13 -0300

    first commit

--- src/com/dio/PrimeiroPrograma.java
+++ src/com/dio/PrimeiroPrograma.java
@@ -1,12 +1,27 @@
 package com.dio;
 
+import com.dio.model.Gato;
+
 public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
-		int a = 2;
+		
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+	
+		
+		/*int a = 2;
 		int b = 3;
-		System.out.println("Hello World! " + (a+b));
+		System.out.println("Hello World! " + (a+b));*/
 
 	}
 
+}
+
+class Livros {
+	private String nome;
+	private String npag;
 }
